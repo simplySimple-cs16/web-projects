4ae3c7deaa259f85bb2d38b737929a07b61f62dd
simplySimple-cs16 <tensedstudent123654@gmail.com>
2020-07-15 19:08:56 +0530

    fundas updated

--- jquery/index.js
+++ jquery/index.js
@@ -1,1 +1,9 @@
-alert("Hello")
+$(document).keypress(function(event) {
+  var txt = event.key;
+  $("h1").text(txt);
+
+});
+
+$("h1").on("mouseover",function(){
+  $("h1").css("color","green");
+})
