fca0f91c15c201954202900f6b649574e1fbcb7b
simplySimple-cs16 <tensedstudent123654@gmail.com>
2020-07-18 14:44:36 +0530

    fundas updated

---
+++ intro-to-node/index.js
@@ -0,0 +1,6 @@
+const superheroes = require("superheroes");
+
+console.log("Good: "+superheroes.random());
+
+const supervillains = require('supervillains');
+cons ole.log("Bad: "+supervillains.random());
