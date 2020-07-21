9951dfc9109539c611afa1b57689e18e22d9af71
simplySimple-cs16 <tensedstudent123654@gmail.com>
2020-07-21 14:03:28 +0530

    fundas updated

--- back/Calculator/calculator.js
+++ back/Calculator/calculator.js
@@ -19,6 +19,17 @@ app.post("/", function(req,res){
 
 });
 
+app.get("/bmicalculator", function(req,res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req,res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = Math.round(weight/(height*height));
+  res.send("Your BMI is "+bmi);
+})
+
 app.listen(3000, function(){
   console.log("Server started at port 3000");
 });
